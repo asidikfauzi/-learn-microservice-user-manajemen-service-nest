f798382be805576b670504c9ab0f5fdfad1e39d4
asidikfauzi <asidikfauzi@gmail.com>
2023-03-01 15:38:48 +0700

    set up main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService)
+
+  app.useGlobalPipes(new ValidationPipe())
+
+  const port = configService.get('PORT')
+  await app.listen(port)
+  console.log(`User management service running on port ${port}`);
 }
 bootstrap();
