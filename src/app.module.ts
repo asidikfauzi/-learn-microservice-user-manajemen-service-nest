68d4c7a248621dbd7e415aa2a2cdcd8ad9fd6d93
asidikfauzi <asidikfauzi@gmail.com>
2023-03-01 15:38:19 +0700

    set up module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,55 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './services'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    ClientsModule.registerAsync([
+      {
+        name: 'AUTH_SERVICE',
+        imports: [ConfigModule],
+        useFactory: async (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [`${configService.get('RBMQ_URL')}`],
+            queue: `${configService.get('RBMQ_AUTH_QUEUE')}`,
+            queueOptions: {
+              durable: false,
+            },
+          },
+        }),
+        inject: [ConfigService],
+      },
+      {
+        name: 'LOG_SERVICE',
+        imports: [ConfigModule],
+        useFactory: async (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [`${configService.get('RBMQ_URL')}`],
+            queue: `${configService.get('RBMQ_LOG_QUEUE')}`,
+            queueOptions: {
+              durable: false,
+            },
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    // PrismaService,
+    // {
+    //   provide: APP_GUARD,
+    //   useClass:
+    // }
+  ],
 })
 export class AppModule {}
